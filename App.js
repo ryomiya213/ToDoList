9dfcdbc3dafc908eedad55b355f7aa03bf68d2f8
Ryo Miya <ryomiya213@gmail.com>
2020-07-30 23:14:23 +0900

    todoItemクラス作成

--- App.js
+++ App.js
@@ -4,5 +4,24 @@ function App() {
   console.log('test');
 }
 
+class todoItem {
+  constructor(task) {
+    this.task = task;
+    this.taskDone = false;
+  }
+
+  changeTask(task) {
+    this.task = task;
+  }
+
+  done() {
+    this.taskDone = true;
+  }
+
+  undone() {
+    this.taskDone = false;
+  }
+
+}
 
 App();
